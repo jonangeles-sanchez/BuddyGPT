bff14b3f2a6a59fe2438c9a44befc059d542e2b6
jonangeles-sanchez <jonathanas1337@gmail.com>
2023-06-19 13:22:44 -0400

    Make file content extract function

--- buddygpt/buddygpt.py
+++ buddygpt/buddygpt.py
@@ -18,3 +18,10 @@ def connect_to_chatGPT(msg, files):
     openai.api_key = "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"
     completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[structure_message(msg, files)])
     print_message(completion)
+
+def extract_file_content(files):
+    file_contents = []
+    for file in files:
+        with open(file, "r") as f:
+            file_contents.append({"name" : file, "content" : f.read()})
+    return file_contents
